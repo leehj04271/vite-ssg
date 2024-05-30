412826ce9736605d5b6759d3e8910a4136f912f0
lhj <leehj04271@gmail.com>
2024-05-30 17:39:47 +0900

    dfs

--- examples/multiple-pages/src/components/Counter.vue
+++ examples/multiple-pages/src/components/Counter.vue
@@ -1,26 +1,17 @@
-<script setup lang="ts">
-import { ref } from 'vue'
 
-const props = defineProps<{ init?: number }>()
-
-const counter = ref(props.init || 0)
-</script>
 
 <template>
   <div class="counter">
-    <div>Counter: {{ counter }}</div>
-    <button @click="counter += 1">
-      Inc
-    </button>
-    <button @click="counter -= 1">
-      Dec
-    </button>
+    <div>Counter</div>
+
   </div>
+
+  <style>
+    .counter {
+      border: 2px dashed #3eaf7c;
+      padding: 5px 10px 10px 10px;
+    }
+  </style>
+
 </template>
 
-<style scoped>
-.counter {
-  border: 2px dashed #3eaf7c;
-  padding: 5px 10px 10px 10px;
-}
-</style>
